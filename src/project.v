fceb196936a20c4674ce232a5a18fd33f0a86707
wmk7fe <112506007+wmk7fe@users.noreply.github.com>
2024-02-26 23:24:41 -0500

    Update project.v to match johnson counter

--- src/project.v
+++ src/project.v
@@ -16,9 +16,23 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+ wire r;
+ reg[0:7] out;
+ assign r = rst_n;
+ assign uo_out = out;
+
   // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
 
+    always @ (posedge clk or posedge r)
+	begin
+		if (r) 
+			out= 8'b0000_0000;
+		else
+            out={~out[7],out[0:7-1]};   
+	end
+
+endmodule
+    
 endmodule
